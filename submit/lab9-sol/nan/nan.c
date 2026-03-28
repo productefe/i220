4d832093f65af1c684e3c3712ac812e92e230b8f
esurucu <esurucu@cs.binghamton.edu>
2026-03-27 20:40:36 -0400

    completed nan

--- submit/lab9-sol/nan/nan.c
+++ submit/lab9-sol/nan/nan.c
@@ -3,7 +3,7 @@
 #define TODO 9999.99
 
 int main() {
-  double v = TODO;
+  double v = 0.0;
   double x = v/v;
   const char *neq = (x != x) ? "true" : "false";
   printf("v = %g; x = %g; \"%s\" must be \"true\"\n", v, x, neq);
