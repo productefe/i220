489da736198c4328ce1b00120635784a8b7e57b0
esurucu <esurucu@cs.binghamton.edu>
2026-03-27 20:37:26 -0400

    completed int-overflow

--- submit/lab9-sol/int-overflow/int-overflow.c
+++ submit/lab9-sol/int-overflow/int-overflow.c
@@ -15,39 +15,39 @@ int main() {
   assert(sizeof(int) == 4);
 
   const unsigned char c1 = 240;
-  const unsigned char c2 = c1 + TODO;
+  const unsigned char c2 = c1 + 18;
   assert(c2 == 2);
 
   const signed char c3 = 120;
-  const signed char c4 = TODO;
+  const signed char c4 = 10;
   const signed char c5 = c3 + c4;
   assert(c4 > 0 && c5 == -126);
 
-  const signed char c6 = TODO;
+  const signed char c6 = -128;
   const signed char c7 = -c6;
   assert(c6 == c7);
 
   const signed char c8 = -120;
-  const signed char c9 = TODO;
+  const signed char c9 = -9;
   const signed char c10 = c8 + c9;
   assert(c9 < 0 && c10 == 127);
 
   const signed char c11 = -120;
-  const signed char c12 = TODO;
+  const signed char c12 = -128;
   const signed char c13 = c11 + c12;
   assert(c12 < 0 && c13== 8);
 
   //C now allows ' within literals for readability
   const unsigned short s1 = 60'000;
-  const unsigned short s2 = s1 + TODO;
+  const unsigned short s2 = s1 + 5636;
   assert(s2 == 100);
 
-  const signed short s3 = TODO;
+  const signed short s3 = -32768;
   const signed short s4 = -s3;
   assert(s3 == s4);
 
   const signed short s5 = -30'000;
-  const signed short s6 = TODO;
+  const signed short s6 = -2769;
   const signed short s7 = s5 + s6;
   assert(s6 < 0 && s7 == 32'767);
 }
