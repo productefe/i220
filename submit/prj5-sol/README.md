de2d31e44ff838e0eb447b6b1120dc55d2e41463
esurucu <esurucu@cs.binghamton.edu>
2026-04-30 17:28:11 -0400

    completed prj5

---
+++ submit/prj5-sol/README.md
@@ -0,0 +1,4 @@
+# Prject 5
+**`Name:    `** Efe Surucu <br> 
+**`B-Number:`** B01171812  <br>
+**`BU-Email:`** esurucu@binghamton.edu  <br>
--- submit/prj5-sol/cache-sim.c
+++ submit/prj5-sol/cache-sim.c
@@ -1,57 +1,159 @@
-#include "cache-sim.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
+#include "cache-sim.h"
 #include "memalloc.h"
 
-#include <assert.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdio.h>
+typedef struct {
+    bool valid;
+    bool dirty;
+    MemAddr block_addr;
+} Line;
 
-
-/************************** Type Definitions  **************************/
-
-
-//TODO: add auxiliary typedefs, enums, structs as needed
+typedef struct {
+    Line *lines;
+    unsigned count;
+} Set;
 
 struct CacheSimImpl {
-  //TODO
-  char REMOVE_ME;
+    unsigned s;
+    unsigned b;
+    unsigned E;
+    unsigned num_sets;
+    Replacement repl;
+    Set *sets;
 };
 
-/******************** Creation / Destruction Routines ******************/
-
-/** Create and return a new cache-simulation structure for a
- *  cache for main memory with the specified cache parameters params.
- *  No requirement that *params remains valid after this call.
- */
-CacheSim *
-new_cache_sim(const CacheParams *params)
-{
-  //TODO
-  CacheSim *cache = NULL;
-  return cache;
+static MemAddr get_block_addr(CacheSim *cache, MemAddr addr) {
+    return addr & ~((1UL << cache->b) - 1);
 }
 
-/** Free all resources used by cache-simulation structure *cache */
-void
-free_cache_sim(CacheSim *cache)
-{
-  //TODO
+static unsigned get_set_index(CacheSim *cache, MemAddr addr) {
+    return (addr >> cache->b) & ((1UL << cache->s) - 1);
 }
 
-/************************* Simulation Routine **************************/
+static int find_line(CacheSim *cache, Set *set, MemAddr block_addr) {
+    for (unsigned i = 0; i < set->count; i++) {
+        if (set->lines[i].valid && set->lines[i].block_addr == block_addr) {
+            return i;
+        }
+    }
+    return -1;
+}
 
+static void move_to_front(Set *set, unsigned idx) {
+    if (idx == 0) return;
 
-//TODO: add auxiliary function definitions as needed
+    Line temp = set->lines[idx];
 
+    for (unsigned i = idx; i > 0; i--) {
+        set->lines[i] = set->lines[i - 1];
+    }
 
-/** Return result for reading (is_write == false) or writing (is_write == true)
- *  access_addr from cache
- */
-CacheResult
-cache_sim_result(CacheSim *cache, MemAddr access_addr, bool is_write)
-{
-  //TODO
-  CacheResult result = { 0 };
-  return result;
+    set->lines[0] = temp;
+}
+
+static void insert_front(Set *set, unsigned E, Line line) {
+    unsigned limit = set->count;
+
+    if (limit < E) {
+        set->count++;
+    } else {
+        limit = E - 1;
+    }
+
+    for (unsigned i = limit; i > 0; i--) {
+        set->lines[i] = set->lines[i - 1];
+    }
+
+    set->lines[0] = line;
+}
+
+static unsigned pick_victim(CacheSim *cache, Set *set) {
+    if (cache->repl == MRU_R) {
+        return 0;
+    }
+
+    if (cache->repl == LRU_R) {
+        return set->count - 1;
+    }
+
+    return rand() % cache->E;
+}
+
+CacheSim *new_cache_sim(const CacheParams *params) {
+    CacheSim *cache = malloc_chk(sizeof(CacheSim));
+
+    cache->s = params->n_set_index_bits;
+    cache->b = params->n_blk_offset_bits;
+    cache->E = params->n_lines_per_set;
+    cache->repl = params->replacement;
+    cache->num_sets = 1U << cache->s;
+
+    cache->sets = malloc_chk(cache->num_sets * sizeof(Set));
+
+    for (unsigned i = 0; i < cache->num_sets; i++) {
+        cache->sets[i].lines = calloc_chk(cache->E, sizeof(Line));
+        cache->sets[i].count = 0;
+    }
+
+    return cache;
+}
+
+void free_cache_sim(CacheSim *cache) {
+    for (unsigned i = 0; i < cache->num_sets; i++) {
+        free(cache->sets[i].lines);
+    }
+
+    free(cache->sets);
+    free(cache);
+}
+
+CacheResult cache_sim_result(CacheSim *cache, MemAddr access_addr, bool is_write) {
+    CacheResult result = { 0 };
+
+    result.access_addr = access_addr;
+
+    MemAddr block = get_block_addr(cache, access_addr);
+    unsigned set_idx = get_set_index(cache, access_addr);
+    Set *set = &cache->sets[set_idx];
+
+    int idx = find_line(cache, set, block);
+
+    if (idx >= 0) {
+        result.status = CACHE_HIT;
+
+        if (is_write) {
+            set->lines[idx].dirty = true;
+        }
+
+        move_to_front(set, (unsigned)idx);
+        return result;
+    }
+
+    Line new_line;
+    new_line.valid = true;
+    new_line.dirty = is_write;
+    new_line.block_addr = block;
+
+    if (set->count < cache->E) {
+        result.status = CACHE_MISS_WITHOUT_REPLACE;
+        insert_front(set, cache->E, new_line);
+        return result;
+    }
+
+    unsigned victim = pick_victim(cache, set);
+
+    result.status = CACHE_MISS_WITH_REPLACE;
+    result.replace_addr = set->lines[victim].block_addr;
+    result.is_dirty = set->lines[victim].dirty;
+
+    for (unsigned i = victim; i > 0; i--) {
+        set->lines[i] = set->lines[i - 1];
+    }
+
+    set->lines[0] = new_line;
+
+    return result;
 }
